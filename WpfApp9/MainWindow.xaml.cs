5072e26c2ede2fba48d75a97a9f2c51d3ef473fb
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-05-04 16:59:32 +0500

    Добавьте файлы проекта.

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int buttonCount = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        buttonCount++;
+        Button newButton = new Button
+        {
+            Content = "Кнопка " + buttonCount,
+        };
+        Canvas.SetLeft(newButton, e.GetPosition(myCanvas).X);
+        Canvas.SetTop(newButton, e.GetPosition(myCanvas).Y);
+        myCanvas.Children.Add(newButton);
+    }
+}
